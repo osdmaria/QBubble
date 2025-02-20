b5bada7cb9e27e2c4cfc853c99b86a7da890abae
kameliabouamara <74714311+kameliabouamara@users.noreply.github.com>
2025-02-20 16:10:35 +0100

    delete last fichier ui

--- src/view/gameoverwindow.cpp
+++ src/view/gameoverwindow.cpp
@@ -21,18 +21,34 @@ gameOverWindow::~gameOverWindow() {}
 
 void gameOverWindow::setupUi() {
 
+    //Style des boutons
+    QString buttonStyle = "QPushButton {"
+                          "background-color: #31B472;"
+                          "color: #EEFF6A;"
+                          "border-radius: 15px;"
+                          "font-size: 20px;"
+                          "border: 2px solid #EEFF6A;"
+                          "}"
+                          "QPushButton:hover {"
+                          "background-color: #aade90;"
+                          "}"
+                          "QPushButton:pressed {"
+                          "background-color: #6c9956;"
+                          "}";
+
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
     QLabel *label = new QLabel("Game Over", this);
     label->setAlignment(Qt::AlignCenter);
     mainLayout->addWidget(label);
 
     QHBoxLayout *buttonLayout = new QHBoxLayout();
-    QPushButton *replayButton = new QPushButton("Rejouer", this);
-    QPushButton *menuButton = new QPushButton("Menu Principal", this);
+    QPushButton *m_rejouerButton = new QPushButton("Rejouer", this);
+    QPushButton *m_menuPrincipalButton = new QPushButton("Menu Principal", this);
+    m_rejouerButton->setStyleSheet(buttonStyle);
+    m_menuPrincipalButton ->setStyleSheet(buttonStyle);
 
-    buttonLayout->addWidget(replayButton);
-    buttonLayout->addWidget(menuButton);
-
+    buttonLayout->addWidget(m_rejouerButton);
+    buttonLayout->addWidget(m_menuPrincipalButton);
     mainLayout->addLayout(buttonLayout);
 
 
